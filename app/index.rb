bd3e4a404c7c6431785d8469ce83b1017dd65ddb
vexuas <rodriguezgab1601@gmail.com>
2025-01-21 01:26:53 +0800

    Check class if working

--- app/index.rb
+++ app/index.rb
@@ -1,15 +1,24 @@
 require 'discordrb'
 require 'dotenv'
 
+require_relative './command'
+
 Dotenv.load
 
 bot = Discordrb::Bot.new(token: ENV['BOT_TOKEN'], intents: [:servers])
 
-bot.register_application_command(:hello, 'Hello Command', server_id: 1324771533197152390) do
+def hello_handler(event)
+  event.respond(content: 'Hi! owo')
 end
 
-bot.application_command(:hello) do |event|
-  event.respond(content: 'Hi! owo')
+hello_command = Command.new(name: :hello, description: 'Hello Command', command_type: 'misc',
+                            handler: method(:hello_handler))
+
+bot.register_application_command(hello_command.name, hello_command.description, server_id: 1324771533197152390) do
+end
+
+bot.application_command(hello_command.name) do |event|
+  hello_command.execute(event)
 end
 
 bot.run
