98e0299f216a4f37ce91210a48f5cb7937a010a5
vexuas <rodriguezgab1601@gmail.com>
2025-01-21 01:04:14 +0800

    Add method call

--- app/command.rb
+++ app/command.rb
@@ -2,12 +2,16 @@ class Command
   attr_reader name,
               description,
               command_type,
-              execute
+              handler
 
-  def initialize(name:, description:, command_type:, execute:)
+  def initialize(name:, description:, command_type:, handler:)
     @name = name
     @description = description
     @command_type = command_type
-    @execute = execute
+    @handler = handler
+  end
+
+  def execute(*args)
+    @handler.call(*args)
   end
 end
